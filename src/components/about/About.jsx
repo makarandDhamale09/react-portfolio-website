0614d90d228ea167338470b4ff8f0af64c6de799
makaranddhamale <makarand.dhamale09@gmail.com>
2023-05-25 11:07:44 +0530

    Completed About section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,53 @@
 import React from "react";
 import "./about.css";
+import Me from "../../assets/me.jpeg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about_container">
+        <div className="about_me">
+          <div className="about_me-image">
+            <img src={Me} alt="about Image" />
+          </div>
+        </div>
+        <div className="about_content">
+          <div className="about_cards">
+            <article className="about_card">
+              <FaAward className="about_icon" />
+              <h5>Experience</h5>
+              <small>8+ Years Working</small>
+            </article>
+            <article className="about_card">
+              <FiUsers className="about_icon" />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+            <article className="about_card">
+              <VscFolderLibrary className="about_icon" />
+              <h5>Projects</h5>
+              <small>80+ Completed</small>
+            </article>
+          </div>
+          <p>
+            Experienced Software Developer with 8+ years experience in design,
+            testing and maintenance of software systems. Equipped with a diverse
+            and promising skill-set. Able to effectively self-manage during
+            independent projects, as well as collaborate as part of a productive
+            team.
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
