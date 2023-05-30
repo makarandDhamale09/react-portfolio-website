7d8e90e7d4399df6f5daf07a0e4011bab40192c4
makaranddhamale <makarand.dhamale09@gmail.com>
2023-05-30 09:12:13 +0530

    Testimonials added

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,71 @@
 import React from "react";
 import "./testimonials.css";
+import AVTR1 from "../../assets/avatar1.jpg";
+import AVTR2 from "../../assets/avatar2.jpg";
+import AVTR3 from "../../assets/avatar3.jpg";
+import AVTR4 from "../../assets/avatar4.jpg";
+
+// import Swiper core and required modules
+import { Pagination } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
 
 const Testimonials = () => {
-  return <section id="testimonials">Testimonials</section>;
+  const data = [
+    {
+      avatar: AVTR1,
+      name: "Tina Snow",
+      review:
+        "Modi alias animi dolorem aliquam ea eum beatae maiores consectetur prasesentium quibusdam, commodi velit porro blanditiis consequatur qui molestiae. Dolorem, perspicciatis aspernatur labore distinctio ratione delectus voluptatem dolores deserunt explicabo nostrum ducimus quasi?",
+    },
+    {
+      avatar: AVTR2,
+      name: "Shatta Wale",
+      review:
+        "Modi alias animi dolorem aliquam ea eum beatae maiores consectetur prasesentium quibusdam, commodi velit porro blanditiis consequatur qui molestiae. Dolorem, perspicciatis aspernatur labore distinctio ratione delectus voluptatem dolores deserunt explicabo nostrum ducimus quasi?",
+    },
+    {
+      avatar: AVTR3,
+      name: "Kwame Despite",
+      review:
+        "Modi alias animi dolorem aliquam ea eum beatae maiores consectetur prasesentium quibusdam, commodi velit porro blanditiis consequatur qui molestiae. Dolorem, perspicciatis aspernatur labore distinctio ratione delectus voluptatem dolores deserunt explicabo nostrum ducimus quasi?",
+    },
+    {
+      avatar: AVTR4,
+      name: "Nana Ama McBrown",
+      review:
+        "Modi alias animi dolorem aliquam ea eum beatae maiores consectetur prasesentium quibusdam, commodi velit porro blanditiis consequatur qui molestiae. Dolorem, perspicciatis aspernatur labore distinctio ratione delectus voluptatem dolores deserunt explicabo nostrum ducimus quasi?",
+    },
+  ];
+  return (
+    <section id="testimonials">
+      <h5>Review from Clients</h5>
+      <h2>Testimonials</h2>
+      <Swiper
+        className="container testimonials_container"
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        {data.map(({ avatar, name, review }, index) => {
+          return (
+            <SwiperSlide key={index} className="testimonial">
+              <div className="client_avatar">
+                <img src={avatar} />
+              </div>
+              <h5 className="client_name">{name}</h5>
+              <small className="client_review">{review}</small>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </section>
+  );
 };
 
 export default Testimonials;
