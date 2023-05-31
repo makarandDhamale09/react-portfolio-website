0e942ac7af5f297f2df9daec87cfd96103e61e11
makaranddhamale <makarand.dhamale09@gmail.com>
2023-05-31 09:35:41 +0530

    Final changes added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,52 @@
 import React from "react";
 import "./footer.css";
+import { BsLinkedin, BsGithub } from "react-icons/bs";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href="#" className="footer_logo">
+        Makarand
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Service</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="footer_socials">
+        <a
+          href="https://www.linkedin.com/in/makarand-dhamale-210a8090/"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+        <a href="https://github.com/makarandDhamale09" target="_blank">
+          <BsGithub />
+        </a>
+      </div>
+      <div className="footer_copyright">
+        <small>&copy; Makarand Dhamale. All rights reserved</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
